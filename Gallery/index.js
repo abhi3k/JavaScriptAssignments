ec60705e1971b6b4ac35ab2ef4714d0dd3e46171
abhi3k <abhisheks71@yahoo.com>
2022-04-11 23:14:17 +0530

    day1 ver1

---
+++ Gallery/index.js
@@ -0,0 +1,27 @@
+// var panel1 = document.getElementById("panel-1");
+// var panel2 = document.getElementById("panel-2");
+// var panel3 = document.getElementById("panel-3");
+// var panel4 = document.getElementById("panel-4");
+// var panel5 = document.getElementById("panel-5");
+
+
+function choose1(){
+   document.getElementById('panel-1').style.cssText = "flex:4;"
+   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
+}
+function choose2(){
+   document.getElementById('panel-2').style.cssText = "flex:4;"
+   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
+}
+function choose3(){
+   document.getElementById('panel-3').style.cssText = "flex:4;"
+   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
+}
+function choose4(){
+   document.getElementById('panel-4').style.cssText = "flex:4;"
+   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
+}
+function choose5(){
+   document.getElementById('panel-5').style.cssText = "flex:4;"
+   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
+}
