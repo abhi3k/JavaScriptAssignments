2b630cb116b74e75dee60143bad0cf8c97b6d1d9
abhi3k <abhisheks71@yahoo.com>
2022-04-13 01:58:24 +0530

    initial checkpoint v1

--- Gallery/index.js
+++ Gallery/index.js
@@ -1,27 +1,48 @@
-// var panel1 = document.getElementById("panel-1");
-// var panel2 = document.getElementById("panel-2");
-// var panel3 = document.getElementById("panel-3");
-// var panel4 = document.getElementById("panel-4");
-// var panel5 = document.getElementById("panel-5");
+var panel1 = document.getElementById("panel-1");
+var panel2 = document.getElementById("panel-2");
+var panel3 = document.getElementById("panel-3");
+var panel4 = document.getElementById("panel-4");
+var panel5 = document.getElementById("panel-5");
+
+// var panels = document.getElementsByClassName("panel");
+// console.log(panels);
+
+// var cls = panel1.classList;
+// console.log(cls);
+
+function resetActivePanel(){
+   panel1.classList.remove("active");
+   panel2.classList.remove("active");
+   panel3.classList.remove("active");
+   panel4.classList.remove("active");
+   panel5.classList.remove("active");
+}
+
+function selectPanel(panelId){
+   resetActivePanel();
+   document.getElementById(panelId).classList.add("active");
+}
 
 
-function choose1(){
-   document.getElementById('panel-1').style.cssText = "flex:4;"
-   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
-}
-function choose2(){
-   document.getElementById('panel-2').style.cssText = "flex:4;"
-   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
-}
-function choose3(){
-   document.getElementById('panel-3').style.cssText = "flex:4;"
-   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
-}
-function choose4(){
-   document.getElementById('panel-4').style.cssText = "flex:4;"
-   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
-}
-function choose5(){
-   document.getElementById('panel-5').style.cssText = "flex:4;"
-   document.getElementById('he1').style.cssText = "opacity:1;font-family:Menlo;"
-}
+
+// function selectPanel(panelNo){
+//    console.log(panelNo);
+//    resetActivePanel(panelNo);
+//    switch(panelNo){
+//       case 1:
+//          panel1.classList.add('active');
+//          break;
+//       case 2:
+//          panel2.classList.add('active');
+//          break;
+//       case 3:
+//          panel3.classList.add('active');
+//          break;
+//       case 4:
+//          panel4.classList.add('active');
+//          break;
+//       case 5:
+//          panel5.classList.add('active');
+//          break;
+//    }
+// }
