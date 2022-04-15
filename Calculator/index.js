897314c63e5234e4dfcd1bfe9cafb8846ab42755
abhi3k <abhisheks71@yahoo.com>
2022-04-15 23:53:48 +0530

    add some initial function

--- Calculator/index.js
+++ Calculator/index.js
@@ -2,6 +2,27 @@
 var displayArea = document.getElementById("display");
 console.log(displayArea);
 
-function btnClicked(digit){
-    displayArea.innerText = displayArea.innerText + digit;
+function isOperator(opt){
+    if(opt == '+' || opt == '-' || opt == '*' || opt == '/' || opt == '%'){
+        return true;
+    }
+    return false;   
+}
+
+function btnClicked(buttonText){
+    if(displayArea.innerText === '0'){
+        if(buttonText != '00'){
+           if(buttonText == '.' || isOperator(buttonText)){
+                displayArea.innerText = displayArea.innerText + buttonText;
+            }else{
+            displayArea.innerText = buttonText;
+            }
+        }
+    }else{
+        if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)){
+
+        }else{
+            displayArea.innerText = displayArea.innerText + buttonText;
+        }
+    }
 }
