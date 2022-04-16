1add6396d6319b0a88c49a903d4a773cde921234
abhi3k <abhisheks71@yahoo.com>
2022-04-16 17:42:48 +0530

    optimize code

--- Calculator/index.js
+++ Calculator/index.js
@@ -10,37 +10,44 @@ function isOperator(opt){
 }
 
 function btnClicked(buttonText){
-    if(buttonText != 'CLR' && buttonText != 'DEL' && buttonText != '='){    
-        if(displayArea.innerText === '0'){
-            if(buttonText != '00'){
-               if(buttonText == '.' || isOperator(buttonText)){
-                    displayArea.innerText = displayArea.innerText + buttonText;
-                }else{
-                displayArea.innerText = buttonText;
-                }
-            }
-        }else{
-            if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)){
-                displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText;
-            }else{
-                displayArea.innerText = displayArea.innerText + buttonText;
-            }
-        }
-    }else{
-        if(buttonText === 'CLR'){
-            displayArea.innerText = '0';
-        }else if(buttonText === 'DEL'){
-            displayArea.innerText = displayArea.innerText.slice(0,-1);
-        }else if(buttonText === '='){
-            try{
-                displayArea.innerText = eval(displayArea.innerText);
-            }catch(error){
-                displayArea.innerText = 0;
-            }
-        }
+    if(displayArea.innerText.length >= 24){ return; }
+
+    if(buttonText === 'CLR'){
+        displayArea.innerText = '0';
+        return;
     }
+
+    if(buttonText === 'DEL'){
+        if(displayArea.innerText !== '0'){
+            displayArea.innerText = displayArea.innerText.slice(0,-1);
+        }
+        return;
+    }
+
+    if(buttonText === '='){
+        displayArea.innerText = eval(displayArea.innerText);
+        return;
+    }
+
+
+    if(displayArea.innerText === '0'){
+        if(buttonText == '0' || buttonText == '00'){
+            return;
+        }
+        
+        if(buttonText == '.' || isOperator(buttonText)){
+            displayArea.innerText = '0' + buttonText;
+            return;
+        }
+        displayArea.innerText = buttonText;
+    }else{
+        if(isOperator(displayArea.innerText[displayArea.innerText.length-1]) && isOperator(buttonText)){
+            displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText;
+            return;
+        }
+        displayArea.innerText = displayArea.innerText + buttonText;
+    }
+
+
+
 }
-
-
-// error to be handled --> decimal character must not be repeating
-//
