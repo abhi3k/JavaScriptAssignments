7810899cdc0f72f9b255f3276cb429b420bfb461
abhi3k <abhisheks71@yahoo.com>
2022-04-16 16:30:33 +0530

    add correct  button response

--- Calculator/index.js
+++ Calculator/index.js
@@ -1,6 +1,6 @@
 
 var displayArea = document.getElementById("display");
-console.log(displayArea);
+// console.log(displayArea);
 
 function isOperator(opt){
     if(opt == '+' || opt == '-' || opt == '*' || opt == '/' || opt == '%'){
@@ -10,19 +10,37 @@ function isOperator(opt){
 }
 
 function btnClicked(buttonText){
-    if(displayArea.innerText === '0'){
-        if(buttonText != '00'){
-           if(buttonText == '.' || isOperator(buttonText)){
-                displayArea.innerText = displayArea.innerText + buttonText;
+    if(buttonText != 'CLR' && buttonText != 'DEL' && buttonText != '='){    
+        if(displayArea.innerText === '0'){
+            if(buttonText != '00'){
+               if(buttonText == '.' || isOperator(buttonText)){
+                    displayArea.innerText = displayArea.innerText + buttonText;
+                }else{
+                displayArea.innerText = buttonText;
+                }
+            }
+        }else{
+            if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)){
+                displayArea.innerText = displayArea.innerText.slice(0,-1) + buttonText;
             }else{
-            displayArea.innerText = buttonText;
+                displayArea.innerText = displayArea.innerText + buttonText;
             }
         }
     }else{
-        if(isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText)){
-
-        }else{
-            displayArea.innerText = displayArea.innerText + buttonText;
+        if(buttonText === 'CLR'){
+            displayArea.innerText = '0';
+        }else if(buttonText === 'DEL'){
+            displayArea.innerText = displayArea.innerText.slice(0,-1);
+        }else if(buttonText === '='){
+            try{
+                displayArea.innerText = eval(displayArea.innerText);
+            }catch(error){
+                displayArea.innerText = 0;
+            }
         }
     }
 }
+
+
+// error to be handled --> decimal character must not be repeating
+//
