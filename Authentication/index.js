20746808a7b19f4c0e12127adb4ccc965cbcc158
abhi3k <abhisheks71@yahoo.com>
2022-05-11 01:03:57 +0530

    done encode/decode

--- Authentication/index.js
+++ Authentication/index.js
@@ -10,27 +10,58 @@ let e2 = document.getElementById('em2')
 let e3 = document.getElementById('em3')
 let e4 = document.getElementById('em4')
 
+const lookup = {
+    'A': 'N', 'B': 'O', 'C': 'P', 'D': 'Q',
+    'E': 'R', 'F': 'S', 'G': 'T', 'H': 'U',
+    'I': 'V', 'J': 'W', 'K': 'X', 'L': 'Y',
+    'M': 'Z', 'N': 'A', 'O': 'B', 'P': 'C',
+    'Q': 'D', 'R': 'E', 'S': 'F', 'T': 'G',
+    'U': 'H', 'V': 'I', 'W': 'J', 'X': 'K',
+    'Y': 'L', 'Z': 'M',
+    a:'x',b:'y',c:'z',d:'a',e:'b',f:'c',g:'d',
+    h:'e',i:'f',j:'g',k:'h',l:'i',m:'j',n:'k',
+    o:'l',p:'m',q:'n',r:'o',s:'p',t:'q',u:'r',
+    v:'s',w:'t',x:'u',y:'v',z:'w',
+    '0':'7','1':'8','2':'9','3':'0','4':'1',
+    '5':'2','6':'3','7':'4','8':'5','9':'6',
+    '-':'*', '_':'#', '@':'$', '#':'$', '$':'+'
+  };
+
+const encode = (input) => {
+    return input.split('').map((chars) => lookup[chars]).join('')
+}
+
+const decode = (coded) => {
+    const upKeys = Object.keys(lookup)
+    const upValues = Object.values(lookup)
+    let encoded = coded.split('').map(chars => {
+        let index = upValues.findIndex(element => element === chars)
+        return upKeys[index]
+    })
+    return encoded.join('')
+}
+
 const signup = () => {
     let inputPassword = inpPassword.value
-    passwordDb = inputPassword
+    passwordDb = encode(inputPassword)
     passCreateValid.style.display = 'block'
     e1.style.display = 'none'
     e2.style.display = 'none'
     e3.style.display = 'none'
     e4.style.display = 'none'
-    console.log(passwordDb)
+    // console.log(passwordDb)
+    // console.log(decode(passwordDb))
 }
 
 const login = () => {
     let inputPassword = loginPass.value
-    if (inputPassword === passwordDb) {
+    if (inputPassword === decode(passwordDb)) {
         loginValid.style.display = 'block'
         loginInvalid.style.display = 'none'
     } else {
         loginInvalid.style.display = 'block'
         loginValid.style.display = 'none'
     }
-
 }
 
 
